462060fc2d66a1fff7bc65394619191491eb29e2
Ruben Epong <rubenjohne@gmail.com>
2012-08-24 15:59:36 -0700

    User model with passwords, and user show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120823065159) do
+ActiveRecord::Schema.define(:version => 20120824074828) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
+    t.string   "encrypted_password"
+    t.string   "salt",               :limit => nil
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
